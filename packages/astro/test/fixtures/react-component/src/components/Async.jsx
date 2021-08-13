8643931544f0a6d1db024ea04c24b1830158a816
Stephen Solka <solka@angel.co>
2021-08-13 13:29:39 -0400

    fix test case

--- packages/astro/test/fixtures/react-component/src/components/Async.jsx
+++ packages/astro/test/fixtures/react-component/src/components/Async.jsx
@@ -1,15 +1,37 @@
-import React, {useState} from 'react';
+/* eslint-disable require-jsdoc */
+import React, { useState, useEffect } from 'react';
 
-function Inner({value}) {
-    return (<h1 id="async-h1">{value}</h1>)
+function Inner({ value, setValue }) {
+  const fetch = () => {
+    let v = value();
+    if (v) {
+      return v;
+    }
+
+    if (import.meta.env.SSR) {
+      throw Promise.resolve('hello').then(setValue);
+    } else {
+      useEffect(() => {
+        Promise.resolve('hello').then(setValue);
+      })
+
+      return "loading";
+    }
+  };
+  const [body] = useState(fetch);
+  return (<h1 id="async-h1">{body}</h1>)
 }
 
-function Async() {
-  const fetchThing = () => {
-      throw Promise.resolve('hello');
-  };
-  const [value] = useState(fetchThing);
-  return (<Inner value={value}></Inner>);
+const cache = {};
+function Async({ cacheKey }) {
+  const [setValue, value] = import.meta.env.SSR ? [
+    (v) => {
+      cache[cacheKey] = v;
+    },
+    () => cache[cacheKey]]
+    : useState();
+
+  return (<Inner value={value} setValue={setValue}></Inner>);
 }
 
 export default Async
